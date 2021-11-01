e04c75cbcbd8e67e8a40e59ff5c825e13adb14c1
yamato090 <tomoki.y.0924@icloud.com>
2021-11-01 08:43:25 +0000

    Rspec　クリア

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -11,8 +11,9 @@ class BooksController < ApplicationController
     @book = Book.new(book_params)
     if @book.save
       flash[:notice] = "Book was successfully created."
-      redirect_to book_path(@book.id)
+      redirect_to book_path(@book)
     else
+      @books = Book.all
       render :index
     end
   end
@@ -27,9 +28,9 @@ class BooksController < ApplicationController
 
   def update
     @book = Book.find(params[:id])
-    if @book.update()
+    if @book.update(book_params)
       flash[:notice] = "Book was successfully updated."
-      redirect_to book_path(@book.id)
+      redirect_to book_path(@book)
     else
       render :edit
     end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  
-  root "homes#top"
-  
   resources :books
+  root :to => 'homes#top'
+  post 'books' => 'books#index'
+  get 'books#index' => 'books#show'
 end
